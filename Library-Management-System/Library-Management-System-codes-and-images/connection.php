7f2e7e8048e31dcc421dc06f00d864f1f226d8c5
Alhassan-rgb555 <Alh2025se@outlook.com>
2026-06-15 18:00:58 +0300

    Delete Library-Management-System directory

--- Library-Management-System/Library-Management-System-codes-and-images/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hostname="localhost";
-$username="root";
-$password="";
-$database="crudbooka4hj";
-$conn=mysqli_connect($hostname,$username,$password,$database) or die(mysqli_error($conn));
-?>
